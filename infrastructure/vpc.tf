77b8413043dd06e35f633add0d4d83f9d42e6657
Andrew Moore <amoore@babbel.com>
2023-08-02 18:57:18 +0200

    Use dynamic availability zones for subnets

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -22,9 +22,9 @@ resource "aws_vpc" "main" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_subnet" "private" {
-  count             = 2
+  count             = length(data.aws_availability_zones.available.names)
   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  #availability_zone = data.aws_availability_zones.available.names[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   vpc_id            = aws_vpc.main.id
   tags = {
     Name = "${var.APP_NAME}-PrivateSubnet-${count.index + 1}"

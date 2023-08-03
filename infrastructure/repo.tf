5386e3eb96166a42dd4f2f34d5f3b257d400499d
Andrew Moore <amoore@babbel.com>
2023-08-03 17:06:44 +0200

    Use federated role for aws oidc for github actions

--- infrastructure/repo.tf
+++ infrastructure/repo.tf
@@ -9,10 +9,9 @@ resource "github_actions_secret" "AWS_ECR_URL" {
 }
 
 resource "github_actions_secret" "AWS_IAM_ROLE_ARN" {
-  repository  = data.github_repository.this.name
-  secret_name = "AWS_IAM_ROLE_ARN"
-  # TODO: use separate role for ci
-  plaintext_value = aws_iam_role.app-garden-of-intelligence.arn
+  repository      = data.github_repository.this.name
+  secret_name     = "AWS_IAM_ROLE_ARN"
+  plaintext_value = aws_iam_role.repo.arn
 }
 
 data "aws_region" "current" {}
@@ -21,4 +20,40 @@ resource "github_actions_secret" "AWS_REGION" {
   repository      = data.github_repository.this.name
   secret_name     = "AWS_REGION"
   plaintext_value = data.aws_region.current.name
+}
+
+# Configuration for allowing github actions to communicate with AWS
+
+# This is already setup on the shared aws account
+data "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_role" "repo" {
+  name = "github-actions-${var.APP_NAME}"
+
+  assume_role_policy = data.aws_iam_policy_document.repo-assume-role.json
+}
+
+data "aws_iam_policy_document" "repo-assume-role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github_actions.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.REPO_PATH}:*"]
+    }
+  }
 }
